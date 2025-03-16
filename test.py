536372a9aeff1a08c625e3ef1d05892907c2f818
goldmaster631026 <goldmaster631026@gmail.com>
2025-03-16 12:53:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from moralis import sol_api
 
 api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6ImY1NjlhZTRlLTdlNjAtNDRhYS04MDMxLWJmNDllZDcxMTU2NCIsIm9yZ0lkIjoiNDI5OTk1IiwidXNlcklkIjoiNDQyMzA2IiwidHlwZUlkIjoiNWM1MmZjMGMtMTgwMS00ZWRjLWI1NDAtN2ViNjk1MzBkMmFhIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3Mzg5Mjc0MTQsImV4cCI6NDg5NDY4NzQxNH0.xqc7QBe_NBQ5od0_NRlw3Z6xicCuv2DqRVtkVqS_r8o"
 params = {
-    "address": "9ak8pEr1HZxcBB2pZGPBFTuhw4VB8zUPLqmFiZKzpump",
+    "address": "9ak8pEr1HZxcBB2pZGPBFTuhw4VB8zUPLqmFiZKzpup",
     "network": "mainnet",
 }
 
